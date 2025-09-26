6f1cae1a83a92ed6709e6206dc1a8e537a57b9e7
jbscript <jabirjaleel97@gmail.com>
2025-09-26 15:30:17 +0530

    feat: add Separator component for improved layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import ProductsGrid from "@/components/product-grid";
+import { Separator } from "@/components/ui/separator";
 import { getImagePath } from "@/lib/image-path";
 import { Sparkles } from "lucide-react";
 import Image from "next/image";
@@ -24,7 +25,7 @@ const socialLinks = [
 export default function Home() {
   return (
     <main className="max-w-[1052px] mt-5 mx-auto">
-      <header className="flex justify-center">
+      <header className="flex justify-center mb-[50px]">
         <nav
           className="flex items-center gap-x-2"
           aria-label="Social media links"
@@ -48,50 +49,45 @@ export default function Home() {
           ))}
         </nav>
       </header>
-      <section className="container flex flex-col items-start text-left">
+      <section className="container flex flex-col items-start text-left text-md max-w-md">
         <Image
           src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/test-clones/9fd2bb1f-ca7e-41ff-8cf3-8f6f98cb5045-alyssax-com/assets/svgs/logo-8.svg"
           alt="Alyssa X Logo"
           width={34}
           height={34}
-          className="self-start mb-8"
+          className="self-start"
         />
-        <div className="flex flex-col text-[#9b9c9c] items-start text-left gap-y-6 max-w-3xl md:max-w-4xl">
-          <h1 className="font-normal  leading-tight tracking-tight">
+        <div className="flex flex-col text-[#9b9c9c] items-start text-left font-medium gap-y-4 max-w-3xl md:max-w-4xl my-2.5">
+          <h1 className="leading-tight tracking-tight">
             I'm <span className="text-white">Alyssa X</span>, a designer,
             developer, and serial maker.
           </h1>
-          <p className="leading-relaxed font-normal">
+          <p className="leading-relaxed">
             As a 🦄 I've built and shipped{" "}
-            <span className="font-medium text-white">12+ products</span> (with
-            over 160K users), been 2 times a Product Hunt{" "}
-            <span className="font-medium text-white">Maker of The Year</span>{" "}
-            finalist, and much more ✌
+            <span className="text-white">12+ products</span> (with over 160K
+            users), been 2 times a Product Hunt{" "}
+            <span className=" text-white">Maker of The Year</span> finalist, and
+            much more ✌
           </p>
           <a
             href="mailto:hi@alyssax.com"
-            className="text-lg font-medium text-primary hover:text-link-hover transition-colors"
+            className="text-lg text-primary hover:text-link-hover transition-colors"
           >
             Hire me <span className="inline-block">↗</span>
           </a>
         </div>
       </section>
 
-      <div className="flex justify-center">
-        <div className="flex items-center gap-5">
-          <div className="relative text-xl font-medium text-white cursor-pointer">
-            Products
-            <span className="absolute left-0 -bottom-[5px] h-[2px] w-full bg-white" />
-          </div>
-          <Link
-            href="/stack"
-            className="flex items-center gap-2 text-xl font-medium text-gray-500 transition-colors duration-200 ease-in-out hover:text-white"
-          >
+      <div className="my-8">
+        <div className="text-sm font-bold flex items-center gap-5 my-4">
+          <div className="relative text-white cursor-pointer">Products</div>
+          <div className="flex items-center gap-2 text-gray-500 transition-colors duration-200 ease-in-out hover:text-white">
             Stack
-            <Sparkles className="h-4 w-4 text-primary" />
-          </Link>
+          </div>
         </div>
+        <Separator />
       </div>
+
       <ProductsGrid />
       {/* 
         
